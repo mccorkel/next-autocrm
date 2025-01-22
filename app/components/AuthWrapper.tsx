405e47bbf389bc3232e8d68c0e714a68a2767e3e
Max McCorkel <mccorkel@gmail.com>
2025-01-21 21:26:52 -0800

    Hide sign up

--- app/components/AuthWrapper.tsx
+++ app/components/AuthWrapper.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Amplify } from "aws-amplify";
-import { Authenticator, Flex, Button } from '@aws-amplify/ui-react';
+import { Authenticator, Flex, Button, Alert } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { PropsWithChildren } from "react";
 import outputs from "@/amplify_outputs.json";
@@ -10,20 +10,31 @@ Amplify.configure(outputs);
 
 export default function AuthWrapper({ children }: PropsWithChildren) {
   return (
-    <Authenticator>
-      {({ signOut }) => (
-        <Flex direction="column">
-          <Flex
-            as="nav"
-            padding="1rem"
-            backgroundColor="white"
-            justifyContent="flex-end"
-          >
-            <Button onClick={signOut}>Sign Out</Button>
+    <>
+      <Alert variation="info" isDismissible={false} hasIcon={true}>
+        Login Information:
+        <br />
+        Admin: admin@example.com / Admin12345!
+        <br />
+        Supervisor: super@example.com / Super12345!
+        <br />
+        Agent: agent@example.com / Agent12345!
+      </Alert>
+      <Authenticator hideSignUp>
+        {({ signOut }) => (
+          <Flex direction="column">
+            <Flex
+              as="nav"
+              padding="1rem"
+              backgroundColor="white"
+              justifyContent="flex-end"
+            >
+              <Button onClick={signOut}>Sign Out</Button>
+            </Flex>
+            <main style={{ padding: "1rem" }}>{children}</main>
           </Flex>
-          <main style={{ padding: "1rem" }}>{children}</main>
-        </Flex>
-      )}
-    </Authenticator>
+        )}
+      </Authenticator>
+    </>
   );
 }
