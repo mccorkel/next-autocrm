1a87b7179c5b7ae86a7a7447ef36a7520ec4f1f4
Max McCorkel <mccorkel@gmail.com>
2025-01-21 20:26:48 -0800

    comment out iam for now

--- amplify/config/iam.ts
+++ amplify/config/iam.ts
@@ -1,23 +1,23 @@
-import { Stack } from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam';
+// import { Stack } from 'aws-cdk-lib';
+// import * as iam from 'aws-cdk-lib/aws-iam';
 
-export function configureIAM(stack: Stack) {
-  // Create role for ADMIN group
-  const adminRole = new iam.Role(stack, 'AdminGroupRole', {
-    assumedBy: new iam.ServicePrincipal('cognito-idp.amazonaws.com'),
-    description: 'Role for ADMIN group in Cognito',
-  });
+// export function configureIAM(stack: Stack) {
+//   // Create role for ADMIN group
+//   const adminRole = new iam.Role(stack, 'AdminGroupRole', {
+//     assumedBy: new iam.ServicePrincipal('cognito-idp.amazonaws.com'),
+//     description: 'Role for ADMIN group in Cognito',
+//   });
 
-  // Add policy to allow Cognito admin actions
-  adminRole.addToPolicy(new iam.PolicyStatement({
-    effect: iam.Effect.ALLOW,
-    actions: [
-      'cognito-idp:ListUsers',
-      'cognito-idp:AdminCreateUser',
-      'cognito-idp:AdminAddUserToGroup'
-    ],
-    resources: ['*']
-  }));
+//   // Add policy to allow Cognito admin actions
+//   adminRole.addToPolicy(new iam.PolicyStatement({
+//     effect: iam.Effect.ALLOW,
+//     actions: [
+//       'cognito-idp:ListUsers',
+//       'cognito-idp:AdminCreateUser',
+//       'cognito-idp:AdminAddUserToGroup'
+//     ],
+//     resources: ['*']
+//   }));
 
-  return adminRole;
-} 
+//   return adminRole;
+// } 
