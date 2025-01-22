0f226c643b51c25c42cbe8a047893cd95b20b00d
Max McCorkel <mccorkel@gmail.com>
2025-01-21 22:33:46 -0800

    auth ui fix

--- app/components/AuthWrapper.tsx
+++ app/components/AuthWrapper.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Amplify } from "aws-amplify";
-import { Authenticator, Flex } from '@aws-amplify/ui-react';
+import { Authenticator, Flex, Alert } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { PropsWithChildren } from "react";
 import outputs from "@/amplify_outputs.json";
@@ -10,14 +10,25 @@ Amplify.configure(outputs);
 
 export default function AuthWrapper({ children }: PropsWithChildren) {
   return (
-    <>
-      <Authenticator hideSignUp>
-        {() => (
-          <Flex direction="column">
-            {children}
-          </Flex>
-        )}
-      </Authenticator>
-    </>
+    <Flex direction="column" minHeight="100vh">
+      <Alert variation="info" isDismissible={false} hasIcon={true}>
+        Login Information:
+        <br />
+        Admin: admin@example.com / Admin12345!
+        <br />
+        Supervisor: super@example.com / Super12345!
+        <br />
+        Agent: agent@example.com / Agent12345!
+      </Alert>
+      <Flex flex="1" justifyContent="center" alignItems="center">
+        <Authenticator hideSignUp>
+          {() => (
+            <Flex direction="column">
+              {children}
+            </Flex>
+          )}
+        </Authenticator>
+      </Flex>
+    </Flex>
   );
 }
