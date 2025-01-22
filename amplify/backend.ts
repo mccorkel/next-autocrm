1490efa697e79bcc928883c9908570f02d5b453f
Max McCorkel <mccorkel@gmail.com>
2025-01-21 20:31:34 -0800

    backend defined

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,14 +3,7 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 // import { configureIAM } from './config/iam';
 
-// export const backend = defineBackend({
-//   auth,
-//   data,
-//   customHandlers: {
-//     onStackCreate: (stack) => {
-//       const adminRole = configureIAM(stack);
-//       // Attach the role to the auth resource
-//       auth.resources.userPool.addToRolePolicy(adminRole);
-//     }
-//   }
-// });
+export const backend = defineBackend({
+  auth,
+  data
+});
