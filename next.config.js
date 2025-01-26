e522c36a6773e2adc2eb6f8251f38676800fe3b9
Max McCorkel <mccorkel@gmail.com>
2025-01-26 13:13:26 -0800

    lets try a next config js file????

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    SERVICE_ACCOUNT_EMAIL: process.env.SERVICE_ACCOUNT_EMAIL,
+    SERVICE_ACCOUNT_PASSWORD: process.env.SERVICE_ACCOUNT_PASSWORD,
+    NEXT_PUBLIC_AWS_USER_POOL_ID: process.env.NEXT_PUBLIC_AWS_USER_POOL_ID,
+    NEXT_PUBLIC_AWS_REGION: process.env.NEXT_PUBLIC_AWS_REGION,
+    AWS_REGION: process.env.AWS_REGION,
+    GRAPHQL_URL: process.env.GRAPHQL_URL
+  }
+}
 
 module.exports = nextConfig
